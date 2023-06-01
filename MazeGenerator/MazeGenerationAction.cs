f0db23c5b813090ec23094315bfc9d123b0d963a
syukoGit <syukodev@outlook.com>
2023-06-01 16:16:59 +0200

    chore: remove unused record

--- MazeGenerator/MazeGenerationAction.cs
+++
@@ -1,9 +0,0 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright project="MazeGenerator" file="MazeGenerationAction.cs" company="SyukoTech">
-//  Copyright (c) SyukoTech. All rights reserved.
-//  </copyright>
-// -----------------------------------------------------------------------
-
-namespace MazeGenerator;
-
-public record MazeGenerationAction((int X, int Y) Position, EDirection Direction, string CursorId);
